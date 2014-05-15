6f2e77ff800b8f2cf7f13cd1a1a04d09926784a1
Brandon-T <JustBrandonT@gmail.com>
2014-05-15 19:04:59 -0400

    Revert "Cross platform support for EIOS"
    This reverts commit 5d6de334fba22d6863312ee0947fc8dfad7e7c4a.

--- src/eios.c
+++ src/eios.c
@@ -1,68 +1,41 @@
 #include "eios.h"
 
-void* loadModule(const char* libname)
-{
-  #if defined _WIN32 || defined_WIN64
-  return LoadLibrary(libname);
-  #else
-  return dlopen(libname, RTLD_LAZY);
-  #endif
-}
-
-int freeModule(void* module)
-{
-  #if defined _WIN32 || defined _WIN64
-  return FreeLibrary(module);
-  #else
-  return dlclose(module);
-  #endif
-}
-
-void* getFuncAddress(void* module, const char* proc_name)
-{
-  #if defined _WIN32 || defined _WIN64
-  return GetProcAddress(module, proc_name);
-  #else
-  return dlsym(module, proc_name);
-  #endif
-}
-
 bool loadEIOSClient(EIOSClient *client, char *path)
 {
   memset(client, 0, sizeof(EIOSClient));
-  client->libHandle = loadModule(path);
-
+  client->libHandle = dlopen(path, RTLD_LAZY);
+  
   if (client->libHandle == NULL)
     return false;
-
-  client->requestTarget = getFuncAddress(client->libHandle, "EIOS_RequestTarget");
-  client->releaseTarget = getFuncAddress(client->libHandle, "EIOS_ReleaseTarget");
-
-  client->getTargetDimensions = getFuncAddress(client->libHandle, "EIOS_GetTargetDimensions");
-  client->getTargetPosition = getFuncAddress(client->libHandle, "EIOS_GetTargetPosition");
-
-  client->getImageBuffer = getFuncAddress(client->libHandle, "EIOS_GetImageBuffer");
-  client->updateImageBuffer = getFuncAddress(client->libHandle, "EIOS_UpdateImageBuffer");
-  client->updateImageBufferBox = getFuncAddress(client->libHandle, "EIOS_UpdateImageBufferBounds");
-
-  client->getMousePosition = getFuncAddress(client->libHandle, "EIOS_GetMousePosition");
-  client->moveMouse = getFuncAddress(client->libHandle, "EIOS_MoveMouse");
-  client->scrollMouse = getFuncAddress(client->libHandle, "EIOS_ScrollMouse");
-  client->holdMouse = getFuncAddress(client->libHandle, "EIOS_HoldMouse");
-  client->releaseMouse = getFuncAddress(client->libHandle, "EIOS_ReleaseMouse");
-  client->isMouseButtonHeld = getFuncAddress(client->libHandle, "EIOS_IsMouseButtonHeld");
-
-  client->sendString = getFuncAddress(client->libHandle, "EIOS_SendString");
-  client->holdKey = getFuncAddress(client->libHandle, "EIOS_HoldKey");
-  client->releaseKey = getFuncAddress(client->libHandle, "EIOS_ReleaseKey");
-  client->isKeyHeld = getFuncAddress(client->libHandle, "EIOS_IsKeyHeld");
-  client->getKeyCode = getFuncAddress(client->libHandle, "EIOS_GetKeyCode");
-
+  
+  client->requestTarget = dlsym(client->libHandle, "EIOS_RequestTarget");
+  client->releaseTarget = dlsym(client->libHandle, "EIOS_ReleaseTarget");
+  
+  client->getTargetDimensions = dlsym(client->libHandle, "EIOS_GetTargetDimensions");
+  client->getTargetPosition = dlsym(client->libHandle, "EIOS_GetTargetPosition");
+  
+  client->getImageBuffer = dlsym(client->libHandle, "EIOS_GetImageBuffer");
+  client->updateImageBuffer = dlsym(client->libHandle, "EIOS_UpdateImageBuffer");
+  client->updateImageBufferBox = dlsym(client->libHandle, "EIOS_UpdateImageBufferBounds");
+  
+  client->getMousePosition = dlsym(client->libHandle, "EIOS_GetMousePosition");
+  client->moveMouse = dlsym(client->libHandle, "EIOS_MoveMouse");
+  client->scrollMouse = dlsym(client->libHandle, "EIOS_ScrollMouse");
+  client->holdMouse = dlsym(client->libHandle, "EIOS_HoldMouse");
+  client->releaseMouse = dlsym(client->libHandle, "EIOS_ReleaseMouse");
+  client->isMouseButtonHeld = dlsym(client->libHandle, "EIOS_IsMouseButtonHeld");
+  
+  client->sendString = dlsym(client->libHandle, "EIOS_SendString");
+  client->holdKey = dlsym(client->libHandle, "EIOS_HoldKey");
+  client->releaseKey = dlsym(client->libHandle, "EIOS_ReleaseKey");
+  client->isKeyHeld = dlsym(client->libHandle, "EIOS_IsKeyHeld");
+  client->getKeyCode = dlsym(client->libHandle, "EIOS_GetKeyCode");
+  
   return true;
 }
 
 void unloadEIOSClient(EIOSClient *client)
 {
-  freeModule(client->libHandle);
+  dlclose(client->libHandle);
   memset(client, 0, sizeof(EIOSClient));
 }
