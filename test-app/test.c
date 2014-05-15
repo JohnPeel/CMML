5d6de334fba22d6863312ee0947fc8dfad7e7c4a
Brandon-T <JustBrandonT@gmail.com>
2014-05-15 18:53:05 -0400

    Cross platform support for EIOS

--- test-app/test.c
+++ test-app/test.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include "target.h"
 #include "eios.h"
@@ -9,21 +8,25 @@ int main(int argc, char **argv)
 {
   EIOSClient client;
 
+  #if defined _WIN32 || defined _WIN64
+  if (loadEIOSClient(&client, "libsmartremote64.dll")) {
+  #else
   if (loadEIOSClient(&client, "./libsmartremote64.so")) {
+  #endif
     printf("Client: %p\n", client.libHandle);
-    
+
     Target target;
     target.kind = EIOSKind;
     target.eiosData.client = &client;
-    
-    SpawnClient spawnClient = dlsym(client.libHandle, "exp_spawnClient");
-    
+
+    SpawnClient spawnClient = getFuncAddress(client.libHandle, "exp_spawnClient");
+
     target.eiosData.target = spawnClient("java", ".", "http://world37.runescape.com/", ",f681985954784915908", 765, 553, "s", NULL, NULL, NULL);
-    
+
     uint32_t width = 0, height = 0;
     getTargetDimensions(&target, &width, &height);
-    printf("%lux%lu\n", width, height);
-    
+    printf("%ux%u\n", width, height);
+
     unloadEIOSClient(&client);
   } else
     printf("Failed to load libsmartremote64...\n");
