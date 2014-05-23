3a86f4eb24f3b6d218ab45500a3c3420c7ac42f4
John Peel <john@dgby.org>
2014-05-22 22:57:23 -0400

    Add typedefs for each eios method to avoid casting warnings

--- include/eios.h
+++ include/eios.h
@@ -13,32 +13,55 @@
 #define stdcall
 #endif // __i386__
 
+typedef void *(*requestTargetT)(char *initargs) stdcall;
+typedef void (*releaseTargetT)(void *target) stdcall;
+
+typedef void (*getTargetDimensionsT)(void *target, uint32_t *width, uint32_t *height) stdcall;
+typedef void (*getTargetPositionT)(void *target, uint32_t *left, uint32_t *top) stdcall;
+
+typedef ColorData *(*getImageBufferT)(void *target) stdcall;
+typedef void (*updateImageBufferT)(void *target) stdcall;
+typedef void (*updateImageBufferBoxT)(void *target, uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2) stdcall;
+
+typedef void (*getMousePositionT)(void *target, uint32_t *x, uint32_t *y) stdcall;
+typedef void (*moveMouseT)(void *target, uint32_t x, uint32_t y) stdcall;
+typedef void (*scrollMouseT)(void *target, uint32_t x, uint32_t y, uint32_t lines) stdcall;
+typedef void (*holdMouseT)(void *target, uint32_t x, uint32_t y, uint32_t button) stdcall;
+typedef void (*releaseMouseT)(void *target, uint32_t x, uint32_t y, uint32_t button) stdcall;
+typedef bool (*isMouseButtonHeldT)(void *target, uint32_t button) stdcall;
+
+typedef void (*sendStringT)(void *target, char *str, uint32_t keywait, uint32_t keymodwait) stdcall;
+typedef void (*holdKeyT)(void *target, uint32_t key) stdcall;
+typedef void (*releaseKeyT)(void *target, uint32_t key) stdcall;
+typedef bool (*isKeyHeldT)(void *target, uint32_t key) stdcall;
+typedef void (*getKeyCodeT)(void *target, char c) stdcall;
+
 typedef struct EIOSClient_t
 {
     void *libHandle;
 
-    void *(*requestTarget)(char *initargs) stdcall;
-    void (*releaseTarget)(void *target) stdcall;
+    requestTargetT requestTarget;
+    releaseTargetT releaseTarget;
 
-    void (*getTargetDimensions)(void *target, uint32_t *width, uint32_t *height) stdcall;
-    void (*getTargetPosition)(void *target, uint32_t *left, uint32_t *top) stdcall;
+    getTargetDimensionsT getTargetDimensions;
+    getTargetPositionT getTargetPosition;
 
-    ColorData *(*getImageBuffer)(void *target) stdcall;
-    void (*updateImageBuffer)(void *target) stdcall;
-    void (*updateImageBufferBox)(void *target, uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2) stdcall;
+	getImageBufferT getImageBuffer;
+	updateImageBufferT updateImageBuffer;
+	updateImageBufferBoxT updateImageBufferBox;
 
-    void (*getMousePosition)(void *target, uint32_t *x, uint32_t *y) stdcall;
-    void (*moveMouse)(void *target, uint32_t x, uint32_t y) stdcall;
-    void (*scrollMouse)(void *target, uint32_t x, uint32_t y, uint32_t lines) stdcall;
-    void (*holdMouse)(void *target, uint32_t x, uint32_t y, uint32_t button) stdcall;
-    void (*releaseMouse)(void *target, uint32_t x, uint32_t y, uint32_t button) stdcall;
-    bool (*isMouseButtonHeld)(void *target, uint32_t button) stdcall;
+	getMousePositionT getMousePosition;
+	moveMouseT moveMouse;
+	scrollMouseT scrollMouse;
+	holdMouseT holdMouse;
+	releaseMouseT releaseMouse;
+	isMouseButtonHeldT isMouseButtonHeld;
 
-    void (*sendString)(void *target, char *str, uint32_t keywait, uint32_t keymodwait) stdcall;
-    void (*holdKey)(void *target, uint32_t key) stdcall;
-    void (*releaseKey)(void *target, uint32_t key) stdcall;
-    bool (*isKeyHeld)(void *target, uint32_t key) stdcall;
-    void (*getKeyCode)(void *target, char c) stdcall;
+	sendStringT sendString;
+	holdKeyT holdKey;
+	releaseKeyT releaseKey;
+	isKeyHeldT isKeyHeld;
+	getKeyCodeT getKeyCode;
 } EIOSClient;
 
 extern bool loadEIOSClient(EIOSClient *client, char *path);
--- src/eios.c
+++ src/eios.c
@@ -8,28 +8,28 @@ bool loadEIOSClient(EIOSClient *client, char *path)
     if (client->libHandle == NULL)
         return false;
 
-    client->requestTarget = getModuleFunc(client->libHandle, "EIOS_RequestTarget");
-    client->releaseTarget = getModuleFunc(client->libHandle, "EIOS_ReleaseTarget");
+    client->requestTarget = (requestTargetT)getModuleFunc(client->libHandle, "EIOS_RequestTarget");
+    client->releaseTarget = (releaseTargetT)getModuleFunc(client->libHandle, "EIOS_ReleaseTarget");
 
-    client->getTargetDimensions = getModuleFunc(client->libHandle, "EIOS_GetTargetDimensions");
-    client->getTargetPosition = getModuleFunc(client->libHandle, "EIOS_GetTargetPosition");
+    client->getTargetDimensions = (getTargetDimensionsT)getModuleFunc(client->libHandle, "EIOS_GetTargetDimensions");
+    client->getTargetPosition = (getTargetPositionT)getModuleFunc(client->libHandle, "EIOS_GetTargetPosition");
 
-    client->getImageBuffer = getModuleFunc(client->libHandle, "EIOS_GetImageBuffer");
-    client->updateImageBuffer = getModuleFunc(client->libHandle, "EIOS_UpdateImageBuffer");
-    client->updateImageBufferBox = getModuleFunc(client->libHandle, "EIOS_UpdateImageBufferBounds");
+    client->getImageBuffer = (getImageBufferT)getModuleFunc(client->libHandle, "EIOS_GetImageBuffer");
+    client->updateImageBuffer = (updateImageBufferT)getModuleFunc(client->libHandle, "EIOS_UpdateImageBuffer");
+    client->updateImageBufferBox = (updateImageBufferBoxT)getModuleFunc(client->libHandle, "EIOS_UpdateImageBufferBounds");
 
-    client->getMousePosition = getModuleFunc(client->libHandle, "EIOS_GetMousePosition");
-    client->moveMouse = getModuleFunc(client->libHandle, "EIOS_MoveMouse");
-    client->scrollMouse = getModuleFunc(client->libHandle, "EIOS_ScrollMouse");
-    client->holdMouse = getModuleFunc(client->libHandle, "EIOS_HoldMouse");
-    client->releaseMouse = getModuleFunc(client->libHandle, "EIOS_ReleaseMouse");
-    client->isMouseButtonHeld = getModuleFunc(client->libHandle, "EIOS_IsMouseButtonHeld");
+    client->getMousePosition = (getMousePositionT)(getMousePositionT)getModuleFunc(client->libHandle, "EIOS_GetMousePosition");
+    client->moveMouse = (moveMouseT)(moveMouseT)getModuleFunc(client->libHandle, "EIOS_MoveMouse");
+    client->scrollMouse = (scrollMouseT)getModuleFunc(client->libHandle, "EIOS_ScrollMouse");
+    client->holdMouse = (holdMouseT)getModuleFunc(client->libHandle, "EIOS_HoldMouse");
+    client->releaseMouse = (releaseMouseT)getModuleFunc(client->libHandle, "EIOS_ReleaseMouse");
+    client->isMouseButtonHeld = (isMouseButtonHeldT)getModuleFunc(client->libHandle, "EIOS_IsMouseButtonHeld");
 
-    client->sendString = getModuleFunc(client->libHandle, "EIOS_SendString");
-    client->holdKey = getModuleFunc(client->libHandle, "EIOS_HoldKey");
-    client->releaseKey = getModuleFunc(client->libHandle, "EIOS_ReleaseKey");
-    client->isKeyHeld = getModuleFunc(client->libHandle, "EIOS_IsKeyHeld");
-    client->getKeyCode = getModuleFunc(client->libHandle, "EIOS_GetKeyCode");
+    client->sendString = (sendStringT)getModuleFunc(client->libHandle, "EIOS_SendString");
+    client->holdKey = (holdKeyT)getModuleFunc(client->libHandle, "EIOS_HoldKey");
+    client->releaseKey = (releaseKeyT)getModuleFunc(client->libHandle, "EIOS_ReleaseKey");
+    client->isKeyHeld = (isKeyHeldT)getModuleFunc(client->libHandle, "EIOS_IsKeyHeld");
+    client->getKeyCode = (getKeyCodeT)getModuleFunc(client->libHandle, "EIOS_GetKeyCode");
 
     return true;
 }
